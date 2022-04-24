dfbc5fbd90e2296ffdf60dbe05a25e068b7f7252
xirang@dtstack.com <xirang@dtstack.com>
2022-04-24 17:49:43 +0800

    [hotfix-55454][oracle] Fix time precision for date types

--- flinkx-connectors/flinkx-connector-oracle/src/main/java/com/dtstack/flinkx/connector/oracle/source/OracleInputFormat.java
+++ flinkx-connectors/flinkx-connector-oracle/src/main/java/com/dtstack/flinkx/connector/oracle/source/OracleInputFormat.java
@@ -3,6 +3,9 @@ package com.dtstack.flinkx.connector.oracle.source;
 import com.dtstack.flinkx.connector.jdbc.source.JdbcInputFormat;
 import com.dtstack.flinkx.connector.jdbc.util.JdbcUtil;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.sql.SQLException;
 import java.sql.Timestamp;
 
 /**
@@ -28,5 +31,35 @@ public class OracleInputFormat extends JdbcInputFormat {
         timeStr = String.format("TO_TIMESTAMP('%s','yyyy-MM-dd HH24:mi:ss.FF6')", timeStr);
 
         return timeStr;
+    }
+
+    @Override
+    protected void queryForPolling(String startLocation) throws SQLException {
+        // 每隔五分钟打印一次，(当前时间 - 任务开始时间) % 300秒 <= 一个间隔轮询周期
+        if ((System.currentTimeMillis() - startTime) % 300000 <= jdbcConf.getPollingInterval()) {
+            LOG.info("polling startLocation = {}", startLocation);
+        } else {
+            LOG.debug("polling startLocation = {}", startLocation);
+        }
+
+        boolean isNumber = StringUtils.isNumeric(startLocation);
+        switch (type) {
+            case TIMESTAMP:
+            case DATE:
+                Timestamp ts =
+                        isNumber
+                                ? new Timestamp(Long.parseLong(startLocation))
+                                : Timestamp.valueOf(startLocation);
+                ps.setTimestamp(1, ts);
+                break;
+            default:
+                if (isNumber) {
+                    ps.setLong(1, Long.parseLong(startLocation));
+                } else {
+                    ps.setString(1, startLocation);
+                }
+        }
+        resultSet = ps.executeQuery();
+        hasNext = resultSet.next();
     }
 }
